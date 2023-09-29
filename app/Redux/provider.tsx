02232e0ae0dfe5446351d670dea0fc4b01341c27
Chebil <aminechebil33@gmail.com>
2023-09-29 02:36:36 +0100

    [FIX]

--- app/Redux/provider.tsx
+++ app/Redux/provider.tsx
@@ -1,8 +1,11 @@
-"use client";
-
+import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-export function Providers({ children }) {
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
   return <Provider store={store}>{children}</Provider>;
 }
