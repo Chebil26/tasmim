35fd55b193ddb5d9823a4626f0429f7641e605e4
Chebil <aminechebil33@gmail.com>
2023-11-25 02:33:17 +0100

    [desc]

--- components/WhatYouGet.tsx
+++ components/WhatYouGet.tsx
@@ -7,28 +7,35 @@ const WhatYouGet: React.FC = () => {
   const items = [
     {
       id: 1,
-      title: "bohohohohoh 1",
+      title: " Une visite 360 degrés",
       description: "Description for Item 1.",
       imageUrl:
         "https://dlomnkdfvdzwzajbgpxu.supabase.co/storage/v1/object/public/dzeko_server_images/cuisine_image87.jpg",
     },
     {
       id: 2,
-      title: "bohohohohoh 2",
+      title: "Un plan d'aménagement",
       description: "Description for Item 2.",
       imageUrl:
         "https://dlomnkdfvdzwzajbgpxu.supabase.co/storage/v1/object/public/dzeko_server_images/cuisine_image87.jpg",
     },
     {
       id: 3,
-      title: "bohohohohoh 3",
+      title: "Des préconisations de réalisation",
       description: "Description for Item 1.",
       imageUrl:
         "https://dlomnkdfvdzwzajbgpxu.supabase.co/storage/v1/object/public/dzeko_server_images/cuisine_image87.jpg",
     },
     {
       id: 4,
-      title: "bohohohohoh 4",
+      title: "Un accompagnement rigoureux",
+      description: "Description for Item 1.",
+      imageUrl:
+        "https://dlomnkdfvdzwzajbgpxu.supabase.co/storage/v1/object/public/dzeko_server_images/cuisine_image87.jpg",
+    },
+    {
+      id: 5,
+      title: "Des visuels de hautes qualité sous différents angles",
       description: "Description for Item 1.",
       imageUrl:
         "https://dlomnkdfvdzwzajbgpxu.supabase.co/storage/v1/object/public/dzeko_server_images/cuisine_image87.jpg",
